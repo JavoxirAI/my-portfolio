993621b5f83be690972836741d396469a83bafed
Javohir <abdukarimjonovjavohir1@gmail.com>
2025-07-06 18:00:18 +0500

    home about contact html added

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('page/', include('page.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def my_profile(request):
-    return render(request, 'index.html')
+    return render(request, 'profile.html')
 
 def teacher_view(request):
     return render(request, 'teacher.html')
